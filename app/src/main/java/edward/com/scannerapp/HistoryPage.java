90ded7c051d1468918dd9d5d7a1a5e5ff10ad8c9
Christopher Edward <scrith.edward23@gmail.com>
2022-01-11 20:13:18 +0700

    add scan history page

--- app/src/main/java/edward/com/scannerapp/HistoryPage.java
+++ app/src/main/java/edward/com/scannerapp/HistoryPage.java
@@ -9,8 +9,10 @@ import com.huawei.hms.ads.BannerAdSize;
 import com.huawei.hms.ads.HwAds;
 import com.huawei.hms.ads.banner.BannerView;
 import com.huawei.hms.ml.scan.HmsScan;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 import java.util.ArrayList;
 
@@ -18,18 +20,23 @@ public class HistoryPage extends AppCompatActivity {
 
     private ArrayList<History> historyList;
     private DatabaseHandler db;
+    private ImageButton btnBack, btnClearHistory;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_history);
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().hide();
+        }
 
         BannerView bannerView = findViewById(R.id.huawei_banner);
         RecyclerView rvHistory = findViewById(R.id.rvHistory);
-
         db = new DatabaseHandler(this);
-
+        btnBack = findViewById(R.id.btnBack);
+        btnClearHistory = findViewById(R.id.btnClearHistory);
         historyList = db.getAllScanHistory();
+
         HistoryAdapter adapter = new HistoryAdapter(historyList);
         rvHistory.setAdapter(adapter);
         rvHistory.setLayoutManager(new LinearLayoutManager(this));
@@ -41,5 +48,23 @@ public class HistoryPage extends AppCompatActivity {
         bannerView.setBannerRefresh(60);
         AdParam adParam = new AdParam.Builder().build();
         bannerView.loadAd(adParam);
+
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent home = new Intent(HistoryPage.this, MainActivity.class);
+                startActivity(home);
+            }
+        });
+
+        btnClearHistory.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                db.clearScanHistoryTable();
+                overridePendingTransition(0, 0);
+                startActivity(getIntent());
+                overridePendingTransition(0, 0);
+            }
+        });
     }
 }
